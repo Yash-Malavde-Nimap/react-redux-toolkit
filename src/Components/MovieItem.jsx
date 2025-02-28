a650d8280e0d839e19bcab4b112460e83e888b0c
Yash-Malavde-Nimap <yash.malavde@nimapinfotech.in>
2025-02-28 11:34:34 +0530

    few more changes in MovieItem.jsx

--- src/Components/MovieItem.jsx
+++ src/Components/MovieItem.jsx
@@ -3,20 +3,17 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { edit_Movie, remove_Movie } from "../redux/movies/movieSlice";
 
-const MovieItem = ({ mov, i }) => {
+const MovieItem = ({ mov }) => {
   const dispatch = useDispatch();
 
-  const [editMovie, setEditMovie] = useState("");
+  const [editMovie, setEditMovie] = useState(mov.title);
   const [edit, setEdit] = useState(false);
 
-  // console.log(editMovie)
-
   const handleDeleteMovie = (id) => {
-    console.log(id);
     if (id) {
       dispatch(remove_Movie(id));
     }
-    setEdit(false)
+    setEdit(false);
   };
 
   const handleEditMovie = (id) => {
@@ -24,14 +21,13 @@ const MovieItem = ({ mov, i }) => {
       const movie = { id: id, title: editMovie };
       dispatch(edit_Movie(movie));
     }
-
     setEdit(false);
   };
 
   const toggleEdit = () => {
     setEdit((prev) => !prev);
   };
-  
+
   return (
     <div
       style={{
@@ -45,31 +41,32 @@ const MovieItem = ({ mov, i }) => {
         borderRadius: "20px",
       }}
     >
-      <p key={i}>{mov.title}</p>
-      {edit && (
+      {edit ? (
         <>
           <input
             type="text"
             value={editMovie}
             onChange={(e) => setEditMovie(e.target.value)}
             style={{
-                padding:'8px',
-                background:'#fff',
-                color:'black',
-                border:'2px solid black'
+              padding: "8px",
+              background: "#fff",
+              color: "black",
+              border: "2px solid black",
             }}
           />
           <button
             style={{
               padding: "8px",
-              background:'green',
-              color:'white'
+              background: "green",
+              color: "white",
             }}
             onClick={() => handleEditMovie(mov.id)}
           >
             Done
           </button>
         </>
+      ) : (
+        <p>{mov.title}</p>
       )}
 
       <button
